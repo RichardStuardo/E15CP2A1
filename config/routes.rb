6ea00daa7ea5d8d09ee9b22d27c330159aa5fd5a
Richard Stuardo <stuardorichard@gmail.com>
2017-11-19 18:48:36 -0300

    80% de completado

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   resources :histories
+  devise_for :users, controllers: {
+        registrations: 'users/registrations'
+      }
 
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
